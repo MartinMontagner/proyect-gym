44221edde45dccaf53ad0a04469e2979e4543cff
MartinMontagner <martinmontagner@estudiante.mdp.utn.edu.ar>
2025-09-26 18:00:18 -0300

    Implement user signup functionality with password hashing and error handling

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,4 +1,8 @@
 'use server';
+import { redirect } from "next/navigation";
+import { createUser } from "@/lib/user";
+import { hashUserPassword } from "@/lib/hash";
+
 export async function signup(prevState,formData) {
     const email = formData.get('email');
     const password = formData.get('password');
@@ -16,4 +20,18 @@ export async function signup(prevState,formData) {
     if (Object.keys(error).length > 0){
         return {errors: error};
     }
+    const hashedPassword = hashUserPassword(password);
+    try{
+        createUser(email,hashedPassword);
+    } catch (error) {
+        if(error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+            return{
+                errors: {
+                    email: 'Parece que ya existe una cuenta para el correo que has elegido'
+                }
+            };
+        }
+        throw error;
+    }
+    redirect('/training');
 }
