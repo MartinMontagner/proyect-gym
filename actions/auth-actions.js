c1426a4a73a39675144e1b1d80c670c99bef82ce
MartinMontagner <martinmontagner@estudiante.mdp.utn.edu.ar>
2025-09-27 11:10:12 -0300

    Refactor signup function to include user ID retrieval and session creation

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -2,6 +2,7 @@
 import { redirect } from "next/navigation";
 import { createUser } from "@/lib/user";
 import { hashUserPassword } from "@/lib/hash";
+import { createAuthSession } from "@/lib/auth";
 
 export async function signup(prevState,formData) {
     const email = formData.get('email');
@@ -22,7 +23,9 @@ export async function signup(prevState,formData) {
     }
     const hashedPassword = hashUserPassword(password);
     try{
-        createUser(email,hashedPassword);
+        const id = createUser(email,hashedPassword);
+        await createAuthSession(id);
+        redirect('/training');
     } catch (error) {
         if(error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
             return{
@@ -33,5 +36,4 @@ export async function signup(prevState,formData) {
         }
         throw error;
     }
-    redirect('/training');
 }
